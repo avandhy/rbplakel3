0dfed4e84e0f229268e80df135fa751ff121d26e
radityarayhanzanesty <85434074+radityarayhanzanesty@users.noreply.github.com>
2021-06-06 18:51:04 +0700

    r

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $question = \App\Models\Question::all();
+        return view('Question/index',['question'=> $question]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('Question/pertanyaan');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        \App\Models\Question::create([
+            'judul'=> $request->get('judul'),
+            'isi_pertanyaan'=> $request->get('isi_pertanyaan'),
+            'gambar_pertanyaan'=> $request->get('gambar_pertanyaan')
+        ]);
+        return redirect('/pertanyaan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $pertanyaan = \App\Models\Question::find($id);
+        return view('Question/edit', ['question' => $question]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $question = \App\Models\question::find($id);
+        $question -> judul = $request -> judul;
+        $question -> isi_jawaban = $request -> isi_jawaban;
+        $question -> save();
+
+        return redirect('/pertanyaan');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $answer = \App\Models\Question::find($id);
+        $answer -> delete();
+        return redirect('/pertanyaan');
+    }
+}
+
+
---
+++ database/migrations/2021_06_06_114658_create_question_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question', function (Blueprint $table) {
+            $table->increments('id_question');
+            $table->string('judul');
+            $table->string('isi_pertanyaan');
+            $table->binary('gambar_pertanyaan')->nullable; 
+            $table->integer('id_user')->references('id')->on('users');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question');
+    }
+}
