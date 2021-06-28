dc06c2d434d82c1d321d4b9c5eb08ba997edc921
avandhy putra <70370100+avandhy@users.noreply.github.com>
2021-06-28 12:54:51 +0700

    view

---
+++ resources/views/Question/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app', ['class' => 'bg-default'])
+
+@section('content')
+    <body>
+        <div class="container">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    Pertanyaan Anda
+                </div>
+                <div class="card-body">
+                    <a href="/Question/create" class="btn btn-primary">Tambah Pertanyaan</a>
+                    <br/>
+                    <br/>
+                    <table class="table table-bordered table-hover table-striped">
+                    <thead>
+                            <tr>
+                                <th>Judul</th>
+                                <th>Deskripsi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($question as $q)
+                            <tr>
+                                <td>{{ $q->judul }}</td>
+                                <td>{{ $q->isi_pertanyaan }}</td>
+                                <td>
+                                    <a href="/Question/edit/{{ $q->idpertanyaan }}" class="btn btn-warning">Edit</a>
+                                    <a href="/Question/hapus/{{ $q->idpertanyaan }}" class="btn btn-danger">Hapus</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+@endsection
---
+++ resources/views/Question/question.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app', ['class' => 'bg-default'])
+
+@section('content')
+    <body >
+        <div class="container">
+            <div class="row " style="margin-top: 50px">
+                <div class="col-md-6 col-md-offset-3 form-container">
+                    <br>
+                    <br>
+                    <a href="/Question" class="btn btn-primary">Kembali</a>
+                    <br>
+                    <br>
+                    <form role="form" method="post" action="/Question/store">
+                        {{ csrf_field() }}
+                        <div class="row">
+                            <div class="col-sm-12 form-group">
+                                <label for="judul"> Judul:</label>
+                                <input type="text" class="form-control" id="judul" name="judul" required>
+                            </div>
+                            <div class="col-sm-12 form-group">
+                                <label for="deskripsi"> Pertanyaan:</label>
+                                <textarea type="textarea" class="form-control" id="isi_pertanyaan" name="isi_pertanyaan" rows="8" required></textarea>
+                            </div>
+                            <div class="col-sm-12 form-group">
+                                <label for="gambar_pertanyaan"> File Pendukung</label>
+                                <input type="file" class="form-control-file" id="gambar_pertanyaan" name="gambar_pertanyaan" rows="1">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 form-group">
+                                <button type="submit" class="btn btn-lg btn-warning btn-block" >Post </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+@endsection
