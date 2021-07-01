f6f6b493a4b53400b7d0e0c7d9c6acd98ab46054
radityarayhanzanesty <85434074+radityarayhanzanesty@users.noreply.github.com>
2021-07-01 12:49:13 +0700

    penyempurnaan forum

--- resources/views/Question/index.blade.php
+++ resources/views/Question/index.blade.php
@@ -1,33 +1,38 @@
 @extends('layouts.app', ['class' => 'bg-default'])
-
+.is-breakable {
+  word-break: break-word;
+}
 @section('content')
     <body>
         <div class="container">
             <div class="card mt-5">
                 <div class="card-header text-center">
-                    Pertanyaan Anda
+                   FORUM
                 </div>
                 <div class="card-body">
                     <a href="/Question/create" class="btn btn-primary">Tambah Pertanyaan</a>
-                    <a href="/forum/read/{id_question}/answer" class="btn btn-primary">Lihat Bagian Jawaban</a>
                     <br/>
                     <br/>
                     <table class="table table-bordered table-hover table-striped">
                     <thead>
-                            <tr>
-                                <th>Judul</th>
-                                <th>Deskripsi</th>
-                            </tr>
+                   
                         </thead>
                         <tbody>
                             @foreach($question as $q)
                             <tr>
-                                <td>{{ $q->judul }}</td>
-                                <td>{{ $q->isi_pertanyaan }}</td>
-                                <td>
-                                    <a href="/Question/edit/{{ $q->id_question }}" class="btn btn-warning">Edit</a>
-                                    <a href="/Question/hapus/{{ $q->id_question }}" class="btn btn-danger">Hapus</a>
-                                </td>
+                            <td style="word-break: break-all">
+                            <div class="container">
+                       
+                            <h1> {{ $q->judul }}</h1>
+                            </div> 
+                            <div class="container">
+                            <p>{{ $q->isi_pertanyaan }}</p>
+                            <a href="/Question/edit/{{ $q->id_question }}" class="btn btn-warning">Edit</a>
+                            <a href="/Question/hapus/{{ $q->id_question }}" class="btn btn-danger">Hapus</a>
+                            <a href="/forum/read/{id_question}/answer" class="btn btn-primary">Jawaban</a>
+                          
+                            </div> 
+                            </td>
                             </tr>
                             @endforeach
                         </tbody>
