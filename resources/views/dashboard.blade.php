e932914324284288de865934ded41ae581f0e78e
radityarayhanzanesty <85434074+radityarayhanzanesty@users.noreply.github.com>
2021-07-02 15:09:46 +0700

    ketinggalan satu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @include('layouts.headers.cards')
-    
+
     <div class="container-fluid mt--7">
         <div class="row">
       
@@ -11,6 +11,16 @@
         <a href="/report" class="btn btn-danger">LAPOR</a>
    
         </div>
+
+        <div class="flex flex-col text-center w-full mb-12">
+          <h1 class="text-4xl font-semibold title-font mb-2.5 text-white">
+            3 Fitur Utama
+          </h1>
+          <h2 class="text-base font-light title-font mx-12 lg:w-full md:w-full sm:w-3/6 sm:mx-auto text-purple">
+            Kamu dapat menyelesaikan tugasmu dengan mudah
+          </h2>
+        </div>
+
     </div>
  
 
