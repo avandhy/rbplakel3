04a1f88e001282da510053ca4e310373586093a5
McRzald <rizaldi8721@gmail.com>
2021-07-08 14:15:58 +0700

    Ketinggalan Sedikit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Marketask') }}</title>
+        <title>{{ config('Marketask', 'Marketask') }}</title>
         <!-- Favicon -->
         <link href="{{ asset('argon') }}/img/brand/icon.png" rel="icon" type="image/png">
         <!-- Fonts -->
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel 8 User Roles and Permissions Tutorial') }}</title>
+    <title>{{ config('Marketask', 'Laravel 8 User Roles and Permissions Tutorial') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
